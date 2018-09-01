e2904010e479b218f486d13239828e2effce8c52
Jesús Cautivo Chacón <cautivo.chacon@gmail.com>
2018-09-01 18:47:32 +0200

    Setup server app

---
+++ server/.eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2016
+    },
+    "rules": {
+        "no-console": [
+            "error",
+            {
+                allow: ["log"]
+            }
+        ],
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
