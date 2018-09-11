ad6d96b3df7939996ee5b6b8e1aa00e37962fdde
Jesús Cautivo Chacón <cautivo.chacon@gmail.com>
2018-09-11 14:50:42 +0200

    Refactor index in actions for use folders structure

--- server/actions/index.js
+++ server/actions/index.js
@@ -1,6 +1,11 @@
-const {userRepository} = require("../infraestructure");
-const {userValidator} = require("../validators");
+const {userRepository, foodRepository} = require("../infraestructure");
+const {userValidator, foodValidator} = require("../validators");
+const {createUser, getUserByExternalId} = require("./User");
+const {createFood} = require("./Food");
 
 module.exports = {
-    createUser: require("./createUser")({userRepository, userValidator})
+    createUser: createUser({userRepository, userValidator}),
+    getUserByExternalId: getUserByExternalId({userRepository}),
+
+    createFood: createFood({foodRepository, foodValidator})
 };
