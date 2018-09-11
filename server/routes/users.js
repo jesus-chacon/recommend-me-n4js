f8307ccfe7ee115b3984864313c19dc967ca1ee2
Jesús Cautivo Chacón <cautivo.chacon@gmail.com>
2018-09-11 14:51:09 +0200

    Add route in user for get a user by externalId

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,10 +4,17 @@ const {check} = require("express-validator/check");
 
 const Tools = require("../tools");
 
-const {createUser} = require("../actions");
+const {createUser, getUserByExternalId} = require("../actions");
 
 router.post("/", [check("externalId").isLength({min: 1})], Tools.checkErrRequest, (req, res) => {
     Tools.manageResponse(res, "Error creating the user", createUser(req.body));
+});
+
+router.get("/:externalId", async (req, res) => {
+    const user = await getUserByExternalId(req.params.externalId);
+
+    if (!user) Tools.notFound(res);
+    else Tools.response(res, user);
 });
 
 module.exports = router;
