5d4d134e1b1916a14723ce3f1e5b938f50084724
Jesús Cautivo Chacón <cautivo.chacon@gmail.com>
2018-09-11 14:52:45 +0200

    Refactor user testing for use tools in test

--- server/test/user.js
+++ server/test/user.js
@@ -2,67 +2,44 @@ const assert = require("assert");
 const request = require("request-promise");
 const randomString = require("randomstring");
 
+const testingTools = require("./tools");
+
 const baseUrl = "http://localhost:9000";
 const apiUrl = "http://localhost:9000/api/v1";
-
-/*function loadOptionsLogued(method, uri, body) {
-    return {
-        method,
-        uri,
-        headers: {"Authorization": `Bearer ${authToken}`},
-        body,
-        json: true
-    };
-}*/
-
-function loadOptionsNoLogued(method, uri, body) {
-    return {
-        method,
-        uri,
-        body,
-        json: true
-    };
-}
-
-function checkResponse(response) {
-    assert.ok(response);
-    assert.ok(response.data);
-    assert.ok(!response.err);
-}
 
 describe("user.js", () => {
     it("Check API is Online", async () => {
         let response;
 
         try {
-            response = await request(loadOptionsNoLogued("GET", `${baseUrl}`));
+            response = await request(testingTools.loadOptionsNoLogued("GET", `${baseUrl}`));
         } catch (err) {
             assert.ok(false);
         }
 
-        checkResponse(response);
+        testingTools.checkResponse(response);
     });
 
     it("Check create data", async () => {
-        const response = await request(loadOptionsNoLogued("POST", `${apiUrl}/private/users`, {
+        const response = await request(testingTools.loadOptionsLogued("POST", `${apiUrl}/private/users`, {
             externalId: randomString.generate(16),
             name: randomString.generate(5)
         }));
 
-        checkResponse(response);
+        testingTools.checkResponse(response);
     });
 
     it("Check not allow duplicated users", async () => {
         const externalId = randomString.generate(16);
 
-        let response = await request(loadOptionsNoLogued("POST", `${apiUrl}/private/users`, {
+        let response = await request(testingTools.loadOptionsLogued("POST", `${apiUrl}/private/users`, {
             externalId
         }));
 
-        checkResponse(response);
+        testingTools.checkResponse(response);
 
         try {
-            response = await request(loadOptionsNoLogued("POST", `${apiUrl}/private/users`, {
+            response = await request(testingTools.loadOptionsLogued("POST", `${apiUrl}/private/users`, {
                 externalId
             }));
 
