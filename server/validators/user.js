cedce3bafa2da9a1530649efe54e86c0153e9a49
Jesús Cautivo Chacón <cautivo.chacon@gmail.com>
2018-09-11 14:45:30 +0200

    Refactor userValidator to use UserRepository abstraction

--- server/validators/user.js
+++ server/validators/user.js
@@ -1,19 +1,7 @@
-module.exports = (driver) => ({
-    existId: (externalId) => {
-        const session = driver.session();
-        let query = "MATCH (u:User {externalId: $externalId}) RETURN u";
+module.exports = ({userRepository}) => ({
+    existId: async (externalId) => {
+        const user = await userRepository.getByExternalId(externalId);
 
-        return session.run(query, {externalId}).then(result => {
-            session.close();
-
-            if (!result) return false;
-            else if (result.records.length == 0) return false;
-            else {
-                const singleRecord = result.records[0];
-                const node = singleRecord.get(0);
-
-                return !!node.properties && node.properties.externalId == externalId;
-            }
-        });
+        return !!user;
     }
 });
