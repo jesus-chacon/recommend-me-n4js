281aaf7d666c490af7840ce5e8f51e483cee4b46
Jesús Cautivo Chacón <cautivo.chacon@gmail.com>
2018-09-16 20:31:41 +0200

    Expose actions for user

--- server/actions/index.js
+++ server/actions/index.js
@@ -1,11 +1,12 @@
 const {userRepository, foodRepository} = require("../infraestructure");
 const {userValidator, foodValidator} = require("../validators");
-const {createUser, getUserByExternalId} = require("./User");
+const {createUser, getUserByExternalId, eatFood} = require("./User");
 const {createFood} = require("./Food");
 
 module.exports = {
     createUser: createUser({userRepository, userValidator}),
     getUserByExternalId: getUserByExternalId({userRepository}),
+    eatFood: eatFood({userRepository, foodRepository}),
 
     createFood: createFood({foodRepository, foodValidator})
 };
