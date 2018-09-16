85fe6ce620cee34a76b92fa4f2924fdd280b72ba
Jesús Cautivo Chacón <cautivo.chacon@gmail.com>
2018-09-16 20:30:40 +0200

    Add method for ensure user and add method for match user and food

--- server/infraestructure/user.js
+++ server/infraestructure/user.js
@@ -1,40 +1,91 @@
 const Tools = require("../tools");
 
-module.exports = (driver) => ({
-    save: (userData) => {
-        const session = driver.session();
-        //"CREATE (a:Person {name: $name}) RETURN a"
-        const query = `CREATE (u:User {${Tools.parseObjectPropertiesToQuery(userData)}}) RETURN u`;
+const save = (driver) => (userData) => {
+    const session = driver.session();
+    //"CREATE (a:Person {name: $name}) RETURN a"
+    const query = `CREATE (u:User {${Tools.parseObjectPropertiesToQuery(userData)}}) RETURN u`;
 
-        return session.run(query, userData).then((result) => {
-            session.close();
+    return session.run(query, userData).then((result) => {
+        session.close();
 
-            const singleRecord = result.records[0];
-            const node = singleRecord.get(0);
+        const singleRecord = result.records[0];
+        const node = singleRecord.get(0);
 
-            return node.properties;
-        });
-    },
-    getByExternalId: (externalId) => {
-        const session = driver.session();
-        let query = "MATCH (u:User {externalId: $externalId}) RETURN u";
+        return node.properties;
+    });
+};
 
-        return session.run(query, {externalId}).then(result => {
-            session.close();
+const getByExternalId = (driver) => (externalId) => {
+    const session = driver.session();
+    let query = "MATCH (u:User {externalId: $externalId}) RETURN u";
 
-            if (!result) return false;
-            else if (result.records.length == 0) return false;
-            else {
-                try {
-                    const singleRecord = result.records[0];
-                    const node = singleRecord.get(0);
+    return session.run(query, {externalId}).then(result => {
+        session.close();
 
-                    return (node.properties.externalId == externalId) ? node.properties : false;
-                } catch (e) {
-                    Tools.err(`Error loading user with externalId ${externalId}`);
-                    return false;
-                }
+        if (!result) return false;
+        else if (result.records.length == 0) return false;
+        else {
+            try {
+                const singleRecord = result.records[0];
+                const node = singleRecord.get(0);
+
+                return (node.properties.externalId == externalId) ? node.properties : false;
+            } catch (e) {
+                Tools.err(`Error loading user with externalId ${externalId}`);
+                return false;
             }
-        });
+        }
+    });
+};
+
+const ensureUser = (driver) => async (userData) => {
+    if (!userData.externalId) {
+        throw new Error("Not ensure user without externalId");
     }
+
+    const user = await getByExternalId(driver)(userData.externalId);
+
+    if (!user) {
+        return save(driver)(userData);
+    } else {
+        return user;
+    }
+};
+
+const matchFood = (driver) => (externalId, foodName) => {
+    const session = driver.session();
+
+    let query = `
+        MATCH (u:User), (f:Food)
+        WHERE u.externalId = $externalId AND f.search = $search
+        CREATE (u)-[e:EAT]->(f)
+        return e
+    `;
+
+    return session.run(query, {externalId, search: foodName.toLowerCase()}).then(result => {
+        session.close();
+
+        if (!result) return false;
+        else if (result.records.length == 0) return false;
+        else {
+            try {
+
+                console.log(result.records, result.records[0].get(0));
+                const singleRecord = result.records[0];
+                const node = singleRecord.get(0);
+
+                return node.properties;
+            } catch (e) {
+                Tools.err(`Error loading user with externalId ${externalId}`);
+                return false;
+            }
+        }
+    });
+};
+
+module.exports = (driver) => ({
+    save: save(driver),
+    getByExternalId: getByExternalId(driver),
+    ensureUser: ensureUser(driver),
+    matchFood: matchFood(driver)
 });
