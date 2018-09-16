7432445c4e5408d71cb55c5ed650e55705a60548
Jesús Cautivo Chacón <cautivo.chacon@gmail.com>
2018-09-16 20:32:13 +0200

    Add endpoint for manage the relation between user and food

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,7 +4,7 @@ const {check} = require("express-validator/check");
 
 const Tools = require("../tools");
 
-const {createUser, getUserByExternalId} = require("../actions");
+const {createUser, getUserByExternalId, eatFood} = require("../actions");
 
 router.post("/", [check("externalId").isLength({min: 1})], Tools.checkErrRequest, (req, res) => {
     Tools.manageResponse(res, "Error creating the user", createUser(req.body));
@@ -15,6 +15,10 @@ router.get("/:externalId", async (req, res) => {
 
     if (!user) Tools.notFound(res);
     else Tools.response(res, user);
+});
+
+router.post("/:externalId", async (req, res) => {
+    Tools.manageResponse(res, "Error creating the relation", eatFood(req.params.externalId, req.query.food));
 });
 
 module.exports = router;
